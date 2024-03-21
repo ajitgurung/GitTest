97e9a7997de143aa4252f0cf76d81b538643e607
ArpanMahatra1999 <arpanmahatra1999ad@gmail.com>
2024-03-21 08:43:29 -0400

    created function to predict output using svc_model.sav

---
+++ predict.py
@@ -0,0 +1,38 @@
+# libraries
+import numpy as np
+import pickle
+from scaler import MinMaxScaler
+
+# warnings removal
+import warnings
+warnings.filterwarnings('ignore')
+
+
+# function to perform prediction
+def model_predict(cycle, weight_gain, hair_growth, skin_darkening, fast_food, follicle_no_l, follicle_no_r):
+
+    # scale cycle
+    cycle_scaler = MinMaxScaler(2, 5)
+    cycle = cycle_scaler.transform(cycle)
+
+    # scale follicle_no_l
+    fnl_scaler = MinMaxScaler(0, 18)
+    follicle_no_l = fnl_scaler.transform(follicle_no_l)
+
+    # scale follicle_no_r
+    fnr_scaler = MinMaxScaler(0, 20)
+    follicle_no_r = fnr_scaler.transform(follicle_no_r)
+
+    # load models from the .sav file
+    with open('svc_model.sav', 'rb') as file:
+        model = pickle.load(file)
+
+    # reshaping and giving output
+    array = np.array([cycle, weight_gain, hair_growth, skin_darkening, fast_food, follicle_no_l, follicle_no_r])
+    pcos = model.predict(array.reshape(1, -1))
+    if pcos == [1]:
+        return True
+    return False
+
+# predicting test data
+# print(model_predict(2, 1, 1,0, 0, 12, 12))
