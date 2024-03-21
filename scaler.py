e2a4846e57936ae921e9033784ced275a32b0e3b
ArpanMahatra1999 <arpanmahatra1999ad@gmail.com>
2024-03-21 08:37:47 -0400

    changed a comment on MinMaxScaler

--- scaler.py
+++ scaler.py
@@ -1,7 +1,7 @@
 # create min max scaler
 class MinMaxScaler:
 
-    # pass data as list
+    # pass minimum and maximum values
     def __init__(self, min_val, max_val):
         self.min = min_val
         self.max = max_val
